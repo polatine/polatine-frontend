47adb3667db06bd979aa6b18394ca858e51f554b
Polatine <polatinenft@gmail.com>
2022-03-17 19:07:32 +0100

    test

--- pages/mint.js
+++ pages/mint.js
@@ -1,7 +1,7 @@
 import Head from "next/head";
 import { useRouter } from "next/router";
 import { useRouter } from "next/router";
-import { pinFileToIPFS } from "../components/pinata";
+// import { pinFileToIPFS } from "../components/pinata";
 
 export default function Home(props) {
   const mint = async () => {
