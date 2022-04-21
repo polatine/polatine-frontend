7c8159b0556dfde06d26d6af2abc06ef55b541ae
polatine <polatineNFT@gmail.com>
2022-04-21 14:48:31 +0200

    new design standard - initial sketch for create

---
+++ components/NFTCard.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+export default function NFTCard(props) {
+  return (
+    <>
+      <div className="NFTCard">Hello</div>
+
+      <style jsx>{`
+        .NFTCard {
+          background: red;
+          width: 30rem;
+          height: 38rem;
+        }
+      `}</style>
+    </>
+  );
+}
--- pages/create.js
+++ pages/create.js
@@ -10,11 +10,11 @@ import {
   TextField,
 } from "@mui/material/";
 import Head from "next/head";
-import { useRouter } from "next/router";
 import { useState } from "react";
 import Web3 from "web3";
 import { pinFileToIPFS } from "../components/pinata";
 import PageAnimation from "../components/PageAnimation";
+import NFTCard from "../components/NFTCard";
 
 export default function Home(props) {
   const [file, setFile] = useState();
@@ -39,185 +39,53 @@ export default function Home(props) {
         });
     });
   };
-  const router = useRouter();
 
   return (
-    <PageAnimation>
-      <div className="container">
-        <Head>
-          <title>Polatine</title>
-          <link rel="icon" href="/favicon.ico" />
-        </Head>
+    <>
+      <Head>
+        <title>Polatine</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
 
-        <main>
-          <span className="backButton" onClick={() => router.back()}>
-            ⬅
-          </span>
-          <h1 className="title">Create Collection</h1>
-
-          {props.status.connected ? (
-            <div className="mainContainer">
-              <div className="fileInputContainer">
-                {file ? (
-                  <img
-                    className="imgPreview"
-                    src={URL.createObjectURL(file)}
-                    alt="img"
-                    style={{ display: "block", margin: "auto" }}
-                  />
-                ) : (
-                  <div className="imgPreview"></div>
-                )}
-                <label htmlFor="contained-button-file">
-                  <Input
-                    accept="image/*"
-                    id="contained-button-file"
-                    multiple
-                    type="file"
-                    onChange={(event) => setFile(event.target.files[0])}
-                  />
-                  <Button
-                    variant="contained"
-                    component="span"
-                    style={{ display: "block", borderRadius: "0 0 7px 7px" }}
-                  >
-                    Upload Image
-                  </Button>
-                </label>
-              </div>
-
-              <Stack spacing={5} style={{ width: "30%", margin: "auto" }}>
-                <TextField
-                  id="outlined-basic"
-                  onChange={(event) => setName(event.target.value)}
-                  label="Name"
-                  variant="outlined"
-                />
-                <TextField
-                  id="outlined-basic"
-                  onChange={(event) => setDesc(event.target.value)}
-                  label="Description"
-                  variant="outlined"
-                />
-
-                <TextField
-                  id="outlined-basic"
-                  onChange={(event) => {
-                    setCollectionSize(event.target.value);
-                  }}
-                  label="Collection Size"
-                  variant="outlined"
-                />
-
-                <TextField
-                  id="outlined-basic"
-                  onChange={(event) => {
-                    setMintingPrice(event.target.value);
-                  }}
-                  label="Minting Price"
-                  variant="outlined"
-                />
-                <FormControl
-                  sx={{ m: 3 }}
-                  component="fieldset"
-                  variant="standard"
-                >
-                  <FormLabel component="legend">
-                    What are you down todo?
-                  </FormLabel>
-                  <FormGroup>
-                    <FormControlLabel
-                      control={<Checkbox />}
-                      label="Make feature for anybody"
-                    />
-                    <FormControlLabel
-                      control={<Checkbox />}
-                      label="Record inperson"
-                    />
-                    <FormControlLabel
-                      control={<Checkbox />}
-                      label="I have creative control"
-                    />
-                  </FormGroup>
-                </FormControl>
-              </Stack>
-
-              <div className="card" onClick={mint}>
-                <h3>Create collection</h3>
-                <p>Enable your fans to mint your NFTs.</p>
-              </div>
+      <main>
+        {props.status.connected ? (
+          <>
+            <div className="previewView">
+              <NFTCard />
             </div>
-          ) : (
-            <div className="grid" onClick={props.connectWallet}>
-              <div className="card">
-                <h3>Connect wallet</h3>
-                <p>Connect your wallet to proceed</p>
-              </div>
+            <div className="settingsColumn">{/* <NFTCard /> */}</div>
+          </>
+        ) : (
+          <div className="grid" onClick={props.connectWallet}>
+            <div className="card">
+              <h3>Connect wallet</h3>
+              <p>Connect your wallet to proceed</p>
             </div>
-          )}
-        </main>
+          </div>
+        )}
+      </main>
 
-        <style jsx>{`
-          .container {
-            min-height: 100vh;
-            padding: 0 0.5rem;
-            display: flex;
-            flex-direction: column;
-            justify-content: center;
-            align-items: center;
-          }
-          .backButton {
-            position: fixed;
-            font-size: 40pt;
-            left: 100px;
-            top: 40px;
-            cursor: pointer;
-          }
-
-          main {
-            padding: 5rem 0;
-            flex: 1;
-            width: 70%;
-            display: flex;
-            flex-direction: column;
-            justify-content: center;
-            align-items: center;
-          }
-
-          .imgPreview {
-            width: 300px;
-            height: 300px;
-            margin: 0px !important;
-          }
-          div.imgPreview {
-            border: grey 2px solid;
-          }
-
-          .mainContainer {
-            display: flex;
-            width: 100%;
-            justify-content: space-between;
-            flex-direction: row;
-          }
-
-          .fileInputContainer {
-            min-width: 400px;
-            min-height: 300px;
-            // margin-top: 2rem;
-            display: flex;
-            flex-direction: column;
-            // flex-wrap: wrap;
-            // justify-content: space-between;
-            align-items: center;
-          }
-          .textInputsContainer {
-            display: flex;
-            justify-content: space-evenly;
-            width: 600px;
-            margin: 40px;
-          }
-        `}</style>
-      </div>
-    </PageAnimation>
+      <style jsx>{`
+        main {
+          width: 100%;
+          height: 100vh;
+          display: flex;
+          flex-direction: row;
+        }
+        .previewView {
+          min-width: 77%;
+          min-height: 1vh;
+          background: salmon;
+          align-items: center;
+          justify-content: center;
+          display: flex;
+        }
+        .settingsColumn {
+          min-width: 23%;
+          min-height: 100%;
+          background: grey;
+        }
+      `}</style>
+    </>
   );
 }
